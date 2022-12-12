a4205a1057d7988cee373e8b849cf32b8e6ae0af
jasonlai1218 <jasonlai1218@gmail.com>
2022-12-12 22:58:46 +0800

    two sum completed.

--- two_sum.py
+++ two_sum.py
@@ -2,8 +2,18 @@ from typing import List
 
 
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
 
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for i in range(len(nums)):
+            hashmap[nums[i]] = i
+
+        # {2: 0, 7: 1, 11: 2, 15: 3}
+
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashmap and hashmap[complement] != i:
+                return [i, hashmap[complement]]
 
 
 # Press the green button in the gutter to run the script.
@@ -12,16 +22,17 @@ if __name__ == '__main__':
     input_data = [2, 7, 11, 15]
     target = 9
     output = [0, 1]
-    assert output == Solution.twoSum(input_data, target), "validation failed"
+    solution = Solution()
+    assert output == solution.twoSum(input_data, target), "validation failed"
 
     # test case 2
     input_data = [3, 2, 4]
     target = 6
     output = [1, 2]
-    assert output == Solution.twoSum(input_data, target), "validation failed"
+    assert output == solution.twoSum(input_data, target), "validation failed"
 
     # test case 3
     input_data = [3, 3]
     target = 6
     output = [0, 1]
-    assert output == Solution.twoSum(input_data, target), "validation failed"
+    assert output == solution.twoSum(input_data, target), "validation failed"
