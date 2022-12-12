56c575a9b468f3074aaa0cf5edd897d6a381761e
jasonlai1218 <jasonlai1218@gmail.com>
2022-12-13 00:06:23 +0800

    add time complexity and description in README

--- two_sum.py
+++ two_sum.py
@@ -5,18 +5,19 @@ class Solution:
 
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         hashmap = {}
+        # time complexity: O(n)
         for i in range(len(nums)):
             hashmap[nums[i]] = i
 
         # {2: 0, 7: 1, 11: 2, 15: 3}
 
+        # time complexity: O(n)
         for i in range(len(nums)):
             complement = target - nums[i]
             if complement in hashmap and hashmap[complement] != i:
                 return [i, hashmap[complement]]
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # test case 1
     input_data = [2, 7, 11, 15]
